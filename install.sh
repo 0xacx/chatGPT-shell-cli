d11cb84082d5f496985179c76ddd5b51838950df
Achilleas <axilleasxy@gmail.com>
2023-04-08 15:40:48 +0300

    add silent curl option, update comment

--- install.sh
+++ install.sh
@@ -27,10 +27,10 @@ if [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
   fi
 fi
 
-# Installing imgcat if using kitty
+# Installing magick if using kitty
 if [[ "$TERM" == "xterm-kitty" ]]; then
   if [[ ! $(which magick) ]]; then
-    curl https://imagemagick.org/archive/binaries/magick -o /usr/local/bin/magick
+    curl -sS https://imagemagick.org/archive/binaries/magick -o /usr/local/bin/magick
     chmod +x /usr/local/bin/magick
     echo "Installed magick"
   fi
